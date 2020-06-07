880730b8c49f710b0cfb5cc12e054cb1c1eca210
harilet <40688601+harilet@users.noreply.github.com>
2020-06-07 20:52:24 +0530

    Update and rename new.py to Main.py

--- Main.py
+++ Main.py
@@ -5,31 +5,29 @@ from tkinter import *
 import threading
 
 
-arduino = serial.Serial('COM7', 9600, timeout=.1)
-time.sleep(2)
+arduino = serial.Serial('COM7', 9600, timeout=.1) #Create Serial Object
+time.sleep(2) #time to setup connection and wait
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 100
-WAVE_OUTPUT_FILENAME = "output.wav"
+RATE = 44100    #pyaudio variables
 
-p = pyaudio.PyAudio()
+p = pyaudio.PyAudio() #pyaudio object
 
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
-                frames_per_buffer=CHUNK)
+                frames_per_buffer=CHUNK)  #stream object
 
-master = Tk()
+master = Tk() #for GUI
 
 Label(master,text="rate").grid(row=0,column=1)
 Label(master,text="Offset").grid(row=0,column=2)
 
-Label(master,text="red").grid(row=1,column=0)
-DR = Scale(master, from_=0, to=255)
+Label(master,text="red").grid(row=1,column=0)   
+DR = Scale(master, from_=0, to=255) #for red color
 DR.grid(row=1,column=1)
 DR.set(1)
 OR = Scale(master, from_=0, to=255)
@@ -37,7 +35,7 @@ OR.grid(row=1,column=2)
 OR.set(0)
 
 Label(master,text="Green").grid(row=2,column=0)
-DG = Scale(master, from_=0, to=255)
+DG = Scale(master, from_=0, to=255) #for green color
 DG.grid(row=2,column=1)
 DG.set(45)
 OG = Scale(master, from_=0, to=255)
@@ -45,7 +43,7 @@ OG.grid(row=2,column=2)
 OG.set(45)
 
 Label(master,text="Blue").grid(row=3,column=0)
-DB = Scale(master, from_=0, to=255)
+DB = Scale(master, from_=0, to=255) #for blue color
 DB.grid(row=3,column=1)
 DB.set(45)
 OB = Scale(master, from_=0, to=255)
@@ -54,13 +52,17 @@ OB.set(45)
 
 def audio1():
     while True:
-        data = stream.read(CHUNK)
+        data = stream.read(CHUNK)     #read data from mic
         rms = audioop.rms(data, 2)    # here's where you calculate the volume
+        
+        #Calculating red,green,blue values from volume
+        
+        red=int((rms/DR.get())-OR.get()) if DR.get()!=0 else 0  #red
+        gre=int((rms/DG.get())-OG.get()) if DG.get()!=0 else 0  #green
+        blu=int((rms/DB.get())-OB.get()) if DB.get()!=0 else 0  #blue
 
-        red=int((rms/DR.get())-OR.get()) if DR.get()!=0 else 0
-        gre=int((rms/DG.get())-OG.get()) if DG.get()!=0 else 0
-        blu=int((rms/DB.get())-OB.get()) if DB.get()!=0 else 0
-
+        #Limiting and desiding what color to value to put
+        
         red = red if red < 255 else 255
         red = red if red >= 0 else 0
         if red>=255:
@@ -76,12 +78,12 @@ def audio1():
             blu=0
 
         print(rms,":",red,":",gre,":",blu)
-        str1=str(red)+" "+str(gre)+" "+str(blu)
-        arduino.write(str1.encode('utf-8'))
-        arduino.write(("\n").encode('utf-8'))
+        str1=str(red)+" "+str(gre)+" "+str(blu) #Structureing RGB values
+        arduino.write(str1.encode('utf-8')) #sending structured RGB to arduino
+        arduino.write(("\n").encode('utf-8')) #sending newline to indicate end of input
 
 
-x=threading.Thread(target=audio1,args=())
+x=threading.Thread(target=audio1,args=()) #creating thread to read data from mic and passing to arduino
 x.start()
 mainloop()
 
